1d90c4a21c739664d65d0e3808d0cc9f5d73ef1e
Louis-Charles Biron <louirlesbiron6@gmail.com>
2024-09-27 11:45:12 -0400

    sa tu push??

--- app/src/main/java/ecole/ecole/tp22/MainActivity.java
+++ app/src/main/java/ecole/ecole/tp22/MainActivity.java
@@ -1,5 +1,8 @@
 package ecole.ecole.tp22;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +16,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,6 +25,8 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
     List<ImageView> listElkemImg;
+
+    ImageView selectedImg;
 
     int[] imgSrc = new int[]{R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4, R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9, R.drawable.img10};
 
@@ -45,12 +51,33 @@ public class MainActivity extends AppCompatActivity {
         ConstraintLayout myLayout = findViewById(R.id.main);
         myLayout.setOnClickListener(v -> {
             listElkemImg.forEach(x -> x.setImageResource(imgSrc[random(0, imgSrc.length - 1)]));
-            Log.wtf("lol", "change image ajr");
+        });
+
+        listElkemImg.forEach(img->{
+            img.setOnClickListener(v->{
+               // TODO faut pas qu'on puisse select les X
+                selectedImg = img;
+            });
+        });
+
+
+        findViewById(R.id.suivant).setOnClickListener(v->{
+            if (selectedImg != null) startActivity(putImageInIntent(new Intent(this, SuivantActivity.class), selectedImg, "captionTestImgidk"));
         });
     }
 
     public static int random(int min, int max) {
         return new Random().nextInt(max - min + 1) + min;
     }
+
+    private Intent putImageInIntent(Intent intent, ImageView image, String caption){
+        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        ByteArrayOutputStream bs = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, bs);
+        intent.putExtra("bitmap", bs.toByteArray());
+        intent.putExtra("caption", caption);
+        return intent;
+    }
+
 
 }
--- app/src/main/java/ecole/ecole/tp22/SuivantActivity.java
+++ app/src/main/java/ecole/ecole/tp22/SuivantActivity.java
@@ -1,6 +1,9 @@
 package ecole.ecole.tp22;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -8,10 +11,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SuivantActivity extends AppCompatActivity {
 
+    ImageView selectedThing;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.suivant_activity);
+        selectedThing = findViewById(R.id.selectedThing);
+
+
+        byte[] byteArray = getIntent().getByteArrayExtra("bitmap");
+        if (byteArray != null) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            selectedThing.setImageBitmap(bitmap);
+        }
     }
 }

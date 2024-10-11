df91ad172b71f80d8813ce93e533bbaa4803c573
Louis-Charles Biron et Félix Blanchette <felix.blanchette@gmail.com>
2024-10-11 08:19:07 -0400

    ooopps

--- TP2_Partie2/app/src/main/java/naji/ecole/TP23/EnvoyerActivity.java
+++ TP2_Partie2/app/src/main/java/naji/ecole/TP23/EnvoyerActivity.java
@@ -1,8 +1,14 @@
 package naji.ecole.TP23;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EnvoyerActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
